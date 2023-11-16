ff554797c42470eaa9cb640a8bbb18c01c2ab6b8
kusseba <131396062+kusseba@users.noreply.github.com>
2023-11-16 13:29:46 -0300

    Primer parcial de programcion 2

---
+++ src/parcial_01_kus_sebastian/Parcial_01_Kus_Sebastian.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parcial_01_kus_sebastian;
+
+/**
+ *
+ * @author kusse
+ */
+public class Parcial_01_Kus_Sebastian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Parcial_01().setVisible(true);
+    }
+    
+}
